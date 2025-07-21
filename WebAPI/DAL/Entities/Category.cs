e04c1b1c3ca35cdaf6e4beaa6e60193772bd6ec2
eren sezen <erenkaansez17@gmail.com>
2025-07-22 00:01:17 +0300

    Proje dosyası ekle.

---
+++ WebAPI/DAL/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.DAL.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
